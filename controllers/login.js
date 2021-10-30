7846bc04203d4d9caae24fe5dcc60aa55cd8f2a8
Abhijeet Abnave <abhijeetabnave1@gmail.com>
2021-10-31 05:08:05 +0530

    login delete param update

--- controllers/login.js
+++ controllers/login.js
@@ -17,17 +17,23 @@ var postRequest = async function (req, res) {
     const user = await User.findOne({ email });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      const token = jwt.sign(
-        { user_id: user._id, email, role: user.role },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "2h",
-        }
-      );
+      //  const deletedUser = req.body.deleted; // if it's an instance of model
+      //or if it's mongoose model comment line above
+      const deletedUser = user.deleted;
+      if (!deletedUser) {
+        const token = jwt.sign(
+          { user_id: user._id, email, role: user.role },
+          process.env.TOKEN_KEY,
+          {
+            expiresIn: "2h",
+          }
+        );
 
-      user.token = token;
-
-      res.status(200).json(user);
+        user.token = token;
+        res.status(200).json(user);
+      } else {
+        res.status(205).json({ message: "Please reach out to the Admin, this user no more exists" });
+      }
     }
     res.status(400).send("Invalid Credentials");
   } catch (err) {
