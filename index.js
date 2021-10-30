97be0329aa44d7f514065e549ea4b2df6084e758
Abhijeet Abnave <abhijeetabnave1@gmail.com>
2021-10-31 05:09:09 +0530

    new api endpoints

--- index.js
+++ index.js
@@ -15,20 +15,21 @@ var path = require('path');
 
 // var controllerCall = require(path.resolve('.', 'controllers/makeCall.js'));
 var controllerUser = require(path.resolve('.', 'controllers/register.js'));
-var controllerGetUsers = require(path.resolve('.', 'controllers/getUsers.js'));
+var controllerUsers = require(path.resolve('.', 'controllers/users.js'));
 var controllerAuthenticateUser = require(path.resolve('.', 'controllers/login.js'));
 var controllerBlogs = require(path.resolve('.', 'controllers/blogs.js'));
 
 // app.post('/summary', controllerCall.validateRequest, controllerCall.postRequest);
 app.post('/register', controllerUser.validateRequest, controllerUser.postRequest);
 app.post('/login', controllerAuthenticateUser.validateRequest, controllerAuthenticateUser.postRequest);
-app.get('/get-users', auth, controllerGetUsers.getRequest);
-app.post('/post-blogs', auth, controllerBlogs.postRequest);
-app.get('/get-blogs', auth, controllerBlogs.getRequest);
-app.get('/get-all-blogs', auth, controllerBlogs.getAllRequest);
-app.put('/put-blogs', auth, controllerBlogs.putRequest);
-app.put('/approve-blog', auth, controllerBlogs.putApproveRequest);
-app.delete('/delete-blogs', auth, controllerBlogs.deleteRequest);
+app.get('/get-users', auth, controllerUsers.getRequest);
+app.delete('/delete-user', auth, controllerUsers.deleteRequest);
+app.post('/post-blogs', auth, controllerBlogs.validateRequest, controllerBlogs.postRequest);
+app.get('/get-blogs', auth, controllerBlogs.validateRequest, controllerBlogs.getRequest);
+app.get('/get-all-blogs', auth, controllerBlogs.validateRequest, controllerBlogs.getAllRequest);
+app.put('/put-blogs', auth, controllerBlogs.validateRequest, controllerBlogs.putRequest);
+app.put('/approve-blog', auth, controllerBlogs.validateRequest, controllerBlogs.putApproveRequest);
+app.delete('/delete-blogs', auth, controllerBlogs.validateRequest, controllerBlogs.deleteRequest);
 
 
 app.listen(port, function () {
